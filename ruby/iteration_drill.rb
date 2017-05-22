c8eae2a79a65f44723f3c513d45534e33c577bce
Joe Alfaro <josephlalfaro@gmail.com>
2017-05-22 05:58:34 -0700

    5.7

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -6,20 +6,77 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 # 1. Iterate through the zombie_apocalypse_supplies array,
 # printing each item in the array separated by an asterisk
 # ----
+#zombie_apocalypse_supplies.each {|item| puts"#{item} * "}
 
 # 2. In order to keep yourself organized, sort your zombie_apocalypse_supplies
 # in alphabetical order. Do not use any special built-in methods.
 # ----
+def sort(array)
+  unordered = array
+  ordered = []
+  while unordered.length > 0
+    index = 0
+    first_index = 0
+    first = unordered[0]
+    while index < (unordered.length-1)
+      if (first.downcase <= unordered[index+1].downcase)
+        index = index+1
+      else
+        first = unordered[index+1]
+        first_index = index+1
+        index = index +1
+      end
+    end
+    ordered.push(first)
+    unordered.delete_at(first_index)
+  end
+  return ordered
+end
+
+#p sort(zombie_apocalypse_supplies)
 
 # 3. Create a method to see if a particular item (string) is in the
 # zombie_apocalypse_supplies. Do not use any special built-in methods.
 # For instance: are boots in your list of supplies?
 # ----
 
+def search_array(array, item)
+  found = false
+  index = 0
+  while (index < array.length) || found == true
+    if array[index].downcase == item.downcase
+      found = true
+      break
+    else
+      index += 1
+    end
+  end
+  if found == false
+    return "not found"
+  else
+    return index
+  end
+end
+
+p search_array(zombie_apocalypse_supplies, "boots")
+
 # 4. You can't carry too many things, you've only got room in your pack for 5.
 # Remove items in your zombie_apocalypse_supplies in any way you'd like,
 # leaving only 5. Do not use any special built-in methods.
 # ----
+
+def dump_supplies(array)
+  p array
+  new_array = ["", "", "", "", ""]
+  index = 0
+  while index < 5
+    new_array[index] = array[index]
+    index += 1
+  end
+  new_array
+end
+
+p dump_supplies(zombie_apocalypse_supplies)
 
 # 5. You found another survivor! This means you can combine your supplies.
 # Create a new combined supplies list out of your zombie_apocalypse_supplies
@@ -28,6 +85,9 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 # documentation for Arrays.
 other_survivor_supplies = [ "warm clothes", "rations", "compass", "camp stove",
                             "solar battery", "flashlight"]
+
+combined_supplies = zombie_apocalypse_supplies + other_survivor_supplies
+p combined_supplies.uniq
 # ----
 
 # Hash Drills
@@ -45,10 +105,12 @@ extinct_animals = {
 # 1. Iterate through extinct_animals hash, printing each key/value pair
 # with a dash in between the key and value, and an asterisk between each pair.
 # ----
+extinct_animals.each {|animal, year| puts"#{animal} - #{year}"}
 
 # 2. Keep only animals in extinct_animals if they were extinct before
 # the year 2000. Do not use any special built-in methods.
 # ----
+
 
 # 3. Our calculations were completely off, turns out all of those animals went
 # extinct 3 years before the date provided. Update the values in extinct_animals
