e614b777ad60c1c8caa073282df38228c9878de2
Joe Alfaro <josephlalfaro@gmail.com>
2017-05-21 22:12:53 -0700

    5.3 Release 2

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -13,11 +13,11 @@ vehicles = {
 }
 
 p model_types
-model_types.each do |types|
-  puts "#{types}"
+model_types.each do |type|
+  puts "#{type}"
 end
-model_types.map! do |types|
-  types + "s"
+model_types.map! do |type|
+  type + "s"
 end
 p model_types
 
@@ -26,3 +26,23 @@ vehicles.each do |make, models|
   puts "#{make} makes the #{models}"
 end
 p vehicles
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+p numbers.delete_if {|num| num<5}
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+p numbers.keep_if {|num| num<5}
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+p numbers.select {|num| num <5}
+numbers = [1, 2, 3, 4, 1, 3, 5, 8, 9]
+p numbers.drop_while {|num| num <5}
+
+spelled_numbers = {
+  1 => "one",
+  2 => "two",
+  3 => "three",
+  4 => "four",
+  5 => "five"
+}
+p spelled_numbers.delete_if {|digit, word| digit < 5}
+p spelled_numbers.keep_if {|digit, word| digit > 1}
+p spelled_numbers.select {|digit, word| word == "four"}
