2c64194d718de69325fc3b109f726ef5929b091f
Joe Alfaro <josephlalfaro@gmail.com>
2017-05-22 06:17:38 -0700

    add user interface

--- 3Joe_L_Alfaro/calculator.rb
+++ 3Joe_L_Alfaro/calculator.rb
@@ -12,7 +12,16 @@ def calculate (num1, math, num2)
   end
 end
 
-calculate(2,'+',2)
-calculate(2,'-',2)
-calculate(2,'*',2)
-calculate(2,'/',2)
+#calculate(2,'+',2)
+#calculate(2,'-',2)
+#calculate(2,'*',2)
+#calculate(2,'/',2)
+
+puts "Calculate:"
+first_num = gets.chomp.to_i
+puts "Calculate: #{first_num}"
+symbol = gets.chomp
+puts "Calculate: #{first_num} #{symbol}"
+second_num = gets.chomp.to_i
+puts "Calculate: #{first_num} #{symbol} #{second_num}"
+puts calculate(first_num, symbol, second_num)
