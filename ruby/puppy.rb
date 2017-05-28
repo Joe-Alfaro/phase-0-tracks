d40a9c96bc7094375a7f87a0babea6436d1275da
Joe Alfaro <josephlalfaro@gmail.com>
2017-05-28 08:41:25 -0700

    Update 6.1

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,4 +1,12 @@
-#class puppy
-#fur length
-#drooliness
-#tail length
+#species puppy
+
+#fur length: varies
+#drooliness: varies
+#tail length: varies
+#eye count: 2
+#tail count: 1
+#leg count: 4
+
+#jumps
+#wags tail
+#barks
