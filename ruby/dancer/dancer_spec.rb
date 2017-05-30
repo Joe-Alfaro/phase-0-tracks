a6cd42053a0133b6c6c0c05c3745af1d07e40978
Joe Alfaro <josephlalfaro@gmail.com>
2017-05-30 09:21:09 -0700

    6 practice assessment question 2

--- ruby/dancer/dancer_spec.rb
+++ ruby/dancer/dancer_spec.rb
@@ -24,6 +24,14 @@ describe Dancer do
     expect(dancer.age).to eq 34
   end
 
+  it "twirls" do
+    expect(dancer.pirouette).to eq "*twirls*"
+  end
+
+  it "bows to a partner" do
+    expect(dancer.bow).to eq "*bows*"
+  end
+
 #  it "keeps a readable dance card" do
 #    expect(dancer.card).to eq []
 #  end
