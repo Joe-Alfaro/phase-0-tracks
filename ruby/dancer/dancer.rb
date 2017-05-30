cc79047ad532aed745329db70e1441d856875ef1
Joe Alfaro <josephlalfaro@gmail.com>
2017-05-30 09:20:11 -0700

    6 practice assessment question 1

---
+++ ruby/dancer/dancer.rb
@@ -0,0 +1,18 @@
+class Dancer
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+
+  def name
+    @name
+  end
+  def age
+    @age
+  end
+  def age=(new_age)
+    @age = new_age
+  end
+
+end
