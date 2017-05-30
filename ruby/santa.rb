f49b408d0918162d711c6b6f6377c71fa928422f
Joe Alfaro <josephlalfaro@gmail.com>
2017-05-29 21:42:45 -0700

    6.3 release 2

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -18,6 +18,25 @@ class Santa
     puts "Reindeer: #{@reindeer_ranking.join(' ')}"
     puts "Age: #{@age}"
   end
+  def gender
+    @gender
+  end
+  def ethnicity
+    @ethnicity
+  end
+  def age
+    @age
+  end
+  def gender=(new_gender)
+    @gender = new_gender
+  end
+  def celebrate_birthday
+    @age += 1
+  end
+  def get_mad_at(reindeer)
+    @reindeer_ranking.delete_if {|r| r == reindeer}
+    @reindeer_ranking << reindeer
+  end
 end
 
 #santa = Santa.new
@@ -38,13 +57,24 @@ def print_nice(array)
   array.each {|i| puts i.about}
   return array
 end
+
 santas = []
 example_genders = ['Prefer not to state', 'Male', 'Female']
 example_ethnicities = ['Prefer not to state', 'Latino', 'White', 'Black', 'Asian']
+
+#driver code
 counter = 0
-while counter < 10
+while counter < 1
   santas << Santa.new(example_genders[rand(example_genders.length)], example_ethnicities[rand(example_ethnicities.length)])
   counter +=1
 end
 
+print_nice(santas)
+puts "age santa..."
+santas[0].celebrate_birthday
+print_nice(santas)
+puts "bad reindeer..."
+santas[0].get_mad_at("Dasher")
+print_nice(santas)
+santas[0].gender = "Test"
 print_nice(santas)
