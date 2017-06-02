74b5398ae9ea07b121ae23698224da82a40ee208
Joe Alfaro <josephlalfaro@gmail.com>
2017-06-01 20:23:44 -0700

    7.3 Release 3

--- js/data_structures.js
+++ js/data_structures.js
@@ -12,3 +12,19 @@ for (var i = 0; i <= colors.length-1; i++) {
   horse[names[i]] = colors[i];
 }
 // console.log(horse);
+
+function Car(year, make, model) {
+  this.year = year;
+  this.make = make;
+  this.model = model;
+
+  this.rev = function() { console.log("The " + this.year + " " + this.make + " " + this.model + " goes VROOM! VROOM!")};
+}
+
+var hondaAccord = new Car(2003, 'Honda', 'Accord');
+var hondaCivic = new Car(2009, 'Honda', 'Civic SI')
+
+console.log(hondaAccord);
+console.log(hondaCivic);
+hondaAccord.rev();
+hondaCivic.rev();
