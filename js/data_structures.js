19268bf70126b010cdbc561e81f543e9aafb23b2
Joe Alfaro <josephlalfaro@gmail.com>
2017-06-01 20:10:09 -0700

    7.2 Release 1

--- js/data_structures.js
+++ js/data_structures.js
@@ -4,5 +4,11 @@ var names=["Ed","Rocket","Java","Ruby"];
 colors.push("silver");
 names.push("Joe");
 
+/* Driver Code
 console.log(colors);
-console.log(names);
+console.log(names); */
+var horse = {};
+for (var i = 0; i <= colors.length-1; i++) {
+  horse[names[i]] = colors[i];
+}
+// console.log(horse);
