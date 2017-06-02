bbe33392e019a53daeefcb5eaa88cb2d7e3d9d73
Joe Alfaro <josephlalfaro@gmail.com>
2017-06-01 22:28:00 -0700

    7.3 Release 0 correction and Release 2

--- js/algos.js
+++ js/algos.js
@@ -2,12 +2,14 @@
 function longestPhrase(array){
   longest = array[0];
   for (var i = 1; i < array.length; i++) {
-    if (array[i] > array[i-1]){
+    if (array[i].length > longest.length){
       longest = array[i]
     }
   }
   return longest;
 }
+
+/*Convert both objects to arrays so that they can be iterated through and the iteration counter can be used to call the index. compare both arrays and change boolean if equal. */
 function matchCheck(object1, object2){
   var keys1 = Object.keys(object1);
   var values1 = Object.values(object1);
@@ -25,9 +27,32 @@ function matchCheck(object1, object2){
   return matchFound;
 }
 
-//DRIVER CODE
+/*Take an integer and use for FOR loop. in loop use random number to select an index of an alphabet array and push number into string. add string to array. */
+function randomArray(num){
+  array = []
+  alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  for (var x = 0; x < num; x++){
+    string = "";
+    for (var y = (Math.floor(Math.random()*10)) + 1; y > 0; y--){
+      string += alphabet[Math.floor(Math.random() * alphabet.length)]
+    }
+    array.push(string);
+  }
+  return array;
+}
+
+/*DRIVER CODE
 console.log(longestPhrase(["long phrase","longest phrase","longer phrase"]));
 
 console.log(matchCheck({name: "Steven", age: 54}, {name: "Tamir", age: 54}));
 console.log(matchCheck({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3}));
 console.log(matchCheck({name: "Steven", age: 54}, {animal: "Dog", legs: 3}));
+*/
+for (var x=0;x<10;x++){
+  array = randomArray(5);
+  console.log("Out of this array:")
+  console.log(array);
+  string = longestPhrase(array);
+  console.log("This word is the longest:")
+  console.log(string);
+}
